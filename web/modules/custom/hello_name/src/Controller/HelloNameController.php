73e0da829a2950d54d8621609aefbb163a4a2266
TJsohn <sohntaejeong@gmail.com>
2025-09-01 10:51:26 +0300

    in the middle of class 1.9 finish hellonameform

--- web/modules/custom/hello_name/src/Controller/HelloNameController.php
+++ web/modules/custom/hello_name/src/Controller/HelloNameController.php
@@ -3,12 +3,13 @@
 namespace Drupal\hello_name\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Response;
 
 class HelloNameController extends ControllerBase
 {
-    public function hello()
+    public function content()
     {
-        return new Response('Hello - my name is TJ!');
+        return [
+            '#markup' => $this->t('Hello, TJ'),
+        ];
     }
 }
