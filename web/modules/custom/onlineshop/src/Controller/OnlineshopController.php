a7a6d27cd5594dd1758f066f41e72aa51dafc509
TJsohn <sohntaejeong@gmail.com>
2025-09-16 07:20:15 +0300

    fix contoroller in react app

--- web/modules/custom/onlineshop/src/Controller/OnlineshopController.php
+++ web/modules/custom/onlineshop/src/Controller/OnlineshopController.php
@@ -3,7 +3,6 @@
 namespace Drupal\onlineshop\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\Response;
 
 class OnlineshopController extends ControllerBase
 {
